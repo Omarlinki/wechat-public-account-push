ecf3e659010514fa16cb607d9e005f20af0da2b1
LIN <35631731+Omarlinki@users.noreply.github.com>
2022-09-21 22:46:50 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,7 +60,7 @@ export const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "LYJ",
+      name: "老婆",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oCBVz6ss9_iQfgTIsHHN6hFs6_nM",
       // 你想对他发送的模板消息的模板ID
